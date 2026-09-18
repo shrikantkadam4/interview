a9add57228c2613e29ec1492505b5d15047228ca
Shrikant Kadam <shrikantkadam4@gmail.com>
2026-09-18 09:23:41 +0000

    Test

--- src/StreamsApi.java
+++ src/StreamsApi.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class StreamsApi{
     public static void main (String[] args){
         List<String> colors=List.of("red", "green", "blue");
-        List<String> shapes=List.of("circle","triangle","square");
+        List<String> shapes=Arrays.asList("circle","triangle","square");
 
         colors.stream()
                 .flatMap(c -> shapes.stream().map(s -> c + " - " + s))
