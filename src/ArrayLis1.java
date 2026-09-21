cd3c476e464317a66c363a0f7b58e64399623a90
Shrikant Kadam <shrikantkadam4@gmail.com>
2026-09-21 16:46:24 +0530

    Update and rename ArrayList.java to ArrayLis1.java

--- src/ArrayLis1.java
+++ src/ArrayLis1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ArrayList{
+public class ArrayList1{
     public static void main(String[] args){
         List<String> arrayList = Arrays.asList("green", "red", "blue");
         arrayList.stream().forEach(System.out::println);
