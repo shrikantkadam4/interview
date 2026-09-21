851a58161dab90006cc02a79be7c522fa908acaf
Shrikant Kadam <shrikantkadam4@gmail.com>
2026-09-21 16:20:38 +0530

    Add ArrayList2 class to print colors from a list

---
+++ src/ArrayList2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ArrayList2{
+    public static void main(String[] args){
+        List<String> arrayList = Arrays.asList("green", "red", "blue");
+        arrayList.stream().forEach(System.out::println);
+    }
+}
