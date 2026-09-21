ac5436534414bcdcd1f3cfb3960ae40e81276a5d
Shrikant Kadam <shrikantkadam4@gmail.com>
2026-09-21 11:14:23 +0000

    commit

--- src/ArrayList2.java
+++ src/ArrayList2.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class ArrayList2{
     public static void main(String[] args){
-        List<String> arrayList = Arrays.asList("green", "red", "blue");
+        ArrayList<String> arrayList=new ArrayList<String>();
+        
         arrayList.stream().forEach(System.out::println);
     }
 }
