cf5d83c85da610cc0e925262c0ec4a809fc8db02
Shrikant Kadam <shrikantkadam4@gmail.com>
2026-09-21 16:46:04 +0530

    Update and rename ArrayList1.java to ArrayList2.java

--- src/ArrayList2.java
+++ src/ArrayList2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ArrayList1{
+public class ArrayList2{
     public static void main(String[] args){
         List<String> arrayList = List.of("green", "red", "blue");
         arrayList.stream().forEach(System.out::println);
