9d4d9387ee0e2e6cca2bbcc7e3a9914400fcb227
Shrikant Kadam <shrikantkadam4@gmail.com>
2026-09-21 16:45:45 +0530

    Update README with GitHub repo setup instructions

--- src/ArrayList3.java
+++ src/ArrayList3.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ArrayList2{
+public class ArrayList3{
     public static void main(String[] args){
         ArrayList<String> arrayList=new ArrayList<String>();
         
